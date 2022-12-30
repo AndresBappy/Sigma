878a43b5e382d97751a86c4e6c8a0f137e07081c
Bh <bh@axul.net>
2022-12-29 21:36:04 -0500

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+      },
+    },
+    container: {
+      center: true,
+    },
+  },
+  plugins: [
+    // require('@tailwindcss/typography'),
+    require('daisyui'),
+  ],
+};
