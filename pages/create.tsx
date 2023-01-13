1a7ae2011521c386a822fea3c43b5723d25112a9
Andrés Felipe Higuita <higuitac@gmail.com>
2023-01-13 08:00:11 -0500

    Back button added to create

--- pages/create.tsx
+++ pages/create.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 
+import Link from 'next/link';
+import BackIcon from 'public/images/icons/back.svg';
 import BetCreate from 'components/elements/BetCreate';
 
 type Props = {};
 
 const create: React.FC<Props> = (props: Props) => {
   return (
-    <div className="min-h-full flex">
+    <div className="h-full px-2 pt-9">
+      <div className=" pb-5">
+        <Link href="/dashboard" className="btn btn-circle bg-white">
+          <BackIcon />
+        </Link>
+      </div>
+
       <BetCreate />
     </div>
   );
