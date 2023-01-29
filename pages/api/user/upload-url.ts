63633890f5fd3c2c74da32e59fa7e817c004da03
Bh <bh@axul.net>
2023-01-29 12:34:43 -0500

    Update aws variable names

--- pages/api/user/upload-url.ts
+++ pages/api/user/upload-url.ts
@@ -9,10 +9,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const file = req.query.file?.toString() || '';
   const filename = uuidv4() + '-' + new Date().getTime() + '.' + file.substring(file.lastIndexOf('.') + 1, file.length);
   const s3Client = new S3Client({
-    region: process.env.AWS_REGION,
+    region: process.env.AWS_S3_REGION,
     credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY || '',
-      secretAccessKey: process.env.AWS_SECRET_KEY || '',
+      accessKeyId: process.env.AWS_S3_ACCESS_KEY || '',
+      secretAccessKey: process.env.AWS_S3_SECRET_KEY || '',
     },
   });
 
